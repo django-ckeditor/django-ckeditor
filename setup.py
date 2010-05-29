7ab7b32791120616bd74cc5d1bdf1dc1d2a14d93
shaunsephton <shaunsephton@gmail.com>
2010-05-29 10:08:14 +0200

    beter setup

--- setup.py
+++ setup.py
@@ -4,15 +4,21 @@ setup(
     name='django-ckeditor',
     version='0.0.1',
     description='Django admin CKEditor integration.',
+    long_description = open('README.rst', 'r').read(),
     author='Shaun Sephton',
     author_email='shaunsephton@gmail.com',
     url='http://github.com/shaunsephton/django-ckeditor',
     packages = find_packages(),
-    dependency_links = [
-        'http://dist.plone.org/thirdparty/',
-    ],
-    install_requires = [
-        'PIL>=1.1.6',
-    ],
+    dependency_links = ['http://dist.plone.org/thirdparty/',],
+    install_requires = ['PIL>=1.1.6',],
     include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django"
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
 )
