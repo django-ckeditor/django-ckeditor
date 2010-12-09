3383f8928e572db3bde8ef31a233aa8d63d23717
shaunsephton <shaun@praekeltconsulting.com>
2010-12-09 14:57:25 +0200

    pass config_name through

--- ckeditor/fields.py
+++ ckeditor/fields.py
@@ -4,18 +4,19 @@ from django import forms
 from ckeditor.widgets import CKEditorWidget
 
 class RichTextField(models.TextField):
+    def __init__(self, config_name='default', *args, **kwargs):
+        self.config_name = config_name
+        super(RichTextField, self).__init__(*args, **kwargs)
+    
     def formfield(self, **kwargs):
         defaults = {
             'form_class': RichTextFormField,
-            'widget': CKEditorWidget,
+            'config_name': self.config_name,
         }
         defaults.update(kwargs)
         return super(RichTextField, self).formfield(**defaults)
         
-
 class RichTextFormField(forms.fields.Field):
-    widget = CKEditorWidget
-
-    def __init__(self, *args, **kwargs):
-        kwargs.update({'widget': CKEditorWidget})
+    def __init__(self, config_name='default', *args, **kwargs):
+        kwargs.update({'widget': CKEditorWidget(config_name=config_name)})
         super(RichTextFormField, self).__init__(*args, **kwargs)
