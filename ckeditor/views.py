6f66702236432a77358e8a3217dffac93512a514
zip <zip@x201.(none)>
2011-02-27 14:33:01 -0500

    removes multiple slashes from url path only

--- ckeditor/views.py
+++ ckeditor/views.py
@@ -1,4 +1,6 @@
 import os
+import re
+from urlparse import urlparse, urlunparse
 from datetime import datetime
 
 from django.conf import settings
@@ -65,9 +67,13 @@ def get_media_url(path):
         url = upload_prefix + path.replace(settings.CKEDITOR_UPLOAD_PATH, '')
     else:
         url = settings.MEDIA_URL + path.replace(settings.MEDIA_ROOT, '')
+
+    # remove multiple forward-slashes from the path portion of the url
+    url_parts    = list( urlparse( url ) )              # break url into a list
+    url_parts[2] = re.sub( '\/+', '/', url_parts[2] )   # replace two or more slashes with a single slash
+    url = urlunparse( url_parts )                       # reconstruct the url
    
-    # Remove any double slashes.
-    return url.replace('//', '/')
+    return url
 
 def get_upload_filename(upload_name, user):
     # If CKEDITOR_RESTRICT_BY_USER is True upload file to user specific path.
