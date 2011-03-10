eef4eb66e8de99e917fbe9bf8fb939b7978c6df0
Vasili <vasili@techcollective.com>
2011-03-09 20:18:25 -0800

    adding compatibility for South

--- ckeditor/fields.py
+++ ckeditor/fields.py
@@ -7,7 +7,7 @@ class RichTextField(models.TextField):
     def __init__(self, config_name='default', *args, **kwargs):
         self.config_name = config_name
         super(RichTextField, self).__init__(*args, **kwargs)
-    
+
     def formfield(self, **kwargs):
         defaults = {
             'form_class': RichTextFormField,
@@ -15,8 +15,14 @@ class RichTextField(models.TextField):
         }
         defaults.update(kwargs)
         return super(RichTextField, self).formfield(**defaults)
-        
+
 class RichTextFormField(forms.fields.Field):
     def __init__(self, config_name='default', *args, **kwargs):
         kwargs.update({'widget': CKEditorWidget(config_name=config_name)})
         super(RichTextFormField, self).__init__(*args, **kwargs)
+
+try:
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ["^ckeditor\.fields\.RichTextField"])
+except:
+    pass
