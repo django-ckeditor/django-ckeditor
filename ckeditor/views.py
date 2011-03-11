6570607ebcf1ec03251182402e0347acfe647aca
Vasili <vasili@techcollective.com>
2011-03-10 22:28:31 -0800

    refactoring views to allow use of file walking with local paths

--- ckeditor/views.py
+++ ckeditor/views.py
@@ -5,10 +5,10 @@ from django.conf import settings
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-            
-try: 
-    from PIL import Image, ImageOps 
-except ImportError: 
+
+try:
+    from PIL import Image, ImageOps
+except ImportError:
     import Image, ImageOps
 
 try:
@@ -18,9 +18,9 @@ except ImportError:
     # (for compatability with pre-1.1 Djangos)
     def csrf_exempt(fn):
         return fn
-        
+
 THUMBNAIL_SIZE = (75, 75)
-    
+
 def get_available_name(name):
     """
     Returns a filename that's free on the target storage system, and
@@ -45,17 +45,17 @@ def get_thumb_filename(file_name):
 
 def create_thumbnail(filename):
     image = Image.open(filename)
-        
+
     # Convert to RGB if necessary
     # Thanks to Limodou on DjangoSnippets.org
     # http://www.djangosnippets.org/snippets/20/
     if image.mode not in ('L', 'RGB'):
         image = image.convert('RGB')
-       
+
     # scale and crop to thumbnail
     imagefit = ImageOps.fit(image, THUMBNAIL_SIZE, Image.ANTIALIAS)
     imagefit.save(get_thumb_filename(filename))
-        
+
 def get_media_url(path):
     """
     Determine system file's media URL.
@@ -65,7 +65,7 @@ def get_media_url(path):
         url = upload_prefix + path.replace(settings.CKEDITOR_UPLOAD_PATH, '')
     else:
         url = settings.MEDIA_URL + path.replace(settings.MEDIA_ROOT, '')
-   
+
     # Remove any double slashes.
     return url.replace('//', '/')
 
@@ -78,18 +78,18 @@ def get_upload_filename(upload_name, user):
 
     # Generate date based path to put uploaded file.
     date_path = datetime.now().strftime('%Y/%m/%d')
-    
+
     # Complete upload path (upload_path + date_path).
     upload_path = os.path.join(settings.CKEDITOR_UPLOAD_PATH, user_path, date_path)
-   
+
     # Make sure upload_path exists.
     if not os.path.exists(upload_path):
         os.makedirs(upload_path)
-    
+
     # Get available name and return.
     return get_available_name(os.path.join(upload_path, upload_name))
-     
-    
+
+
 @csrf_exempt
 def upload(request):
     """
@@ -101,8 +101,8 @@ def upload(request):
     # Get the uploaded file from request.
     upload = request.FILES['upload']
     upload_ext = os.path.splitext(upload.name)[1]
-   
-    # Open output file in which to store upload. 
+
+    # Open output file in which to store upload.
     upload_filename = get_upload_filename(upload.name, request.user)
     out = open(upload_filename, 'wb+')
 
@@ -120,13 +120,11 @@ def upload(request):
         window.parent.CKEDITOR.tools.callFunction(%s, '%s');
     </script>""" % (request.GET['CKEditorFuncNum'], url))
 
-def get_image_browse_urls(user=None):
+def get_image_files(user=None):
     """
     Recursively walks all dirs under upload dir and generates a list of
-    thumbnail and full image URL's for each file found.
+    full paths for each file found.
     """
-    images = []
-    
     # If a user is provided and CKEDITOR_RESTRICT_BY_USER is True,
     # limit images to user specific path, but not for superusers.
     if user and not user.is_superuser and getattr(settings, 'CKEDITOR_RESTRICT_BY_USER', False):
@@ -135,20 +133,28 @@ def get_image_browse_urls(user=None):
         user_path = ''
 
     browse_path = os.path.join(settings.CKEDITOR_UPLOAD_PATH, user_path)
-    
+
     for root, dirs, files in os.walk(browse_path):
         for filename in [ os.path.join(root, x) for x in files ]:
             # bypass for thumbs
-            if '_thumb' in filename:
+            if os.path.splitext(filename)[0].endswith('_thumb'):
                 continue
-            
-            images.append({
-                'thumb': get_media_url(get_thumb_filename(filename)),
-                'src': get_media_url(filename)
-            })
+            yield filename
+
+def get_image_browse_urls(user=None):
+    """
+    Recursively walks all dirs under upload dir and generates a list of
+    thumbnail and full image URL's for each file found.
+    """
+    images = []
+    for filename in get_image_files(user=user):
+        images.append({
+            'thumb': get_media_url(get_thumb_filename(filename)),
+            'src': get_media_url(filename)
+        })
 
     return images
-    
+
 def browse(request):
     context = RequestContext(request, {
         'images': get_image_browse_urls(request.user),
