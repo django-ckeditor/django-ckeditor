95f3d1e9da1ccbaeb61ad2f21a94620e1a5f2f1e
danols <danols@danols.com>
2011-05-31 10:57:16 -0700

    1. Fixed a bug where instances are over riding each others CKEditor's settings - the latest setting to be loaded is active on all instances. 2. Moved DEAFULT_CONFIG inside the class as in my humble opinion that's where it belongs 3. Added a comment why we modify DEFAULT_CONFIG in render and not in __init__

--- ckeditor/widgets.py
+++ ckeditor/widgets.py
@@ -11,20 +11,22 @@ from django.forms.util import flatatt
 
 json_encode = simplejson.JSONEncoder().encode
 
-DEFAULT_CONFIG = {
-    'skin': 'v2',
-    'toolbar': 'Full',
-    'height': 291,
-    'width': 618,
-    'filebrowserWindowWidth': 940,
-    'filebrowserWindowHeight': 747,
-}
-
 class CKEditorWidget(forms.Textarea):
     """
     Widget providing CKEditor for Rich Text Editing.
     Supports direct image uploads and embed.
     """
+    DEFAULT_CONFIG = {
+        'skin': 'v2',
+        'toolbar': 'Full',
+        'height': 291,
+        'width': 618,
+        'filebrowserWindowWidth': 940,
+        'filebrowserWindowHeight': 747,
+        # later on we initialize 'filebrowserUploadUrl' and 'filebrowserBrowseUrl' which are dynamic
+    }
+
+    
     class Media:
         try:
             js = (
@@ -35,8 +37,7 @@ class CKEditorWidget(forms.Textarea):
 
     def __init__(self, config_name='default', *args, **kwargs):
         super(CKEditorWidget, self).__init__(*args, **kwargs)
-        # Setup config from defaults.
-        self.config = DEFAULT_CONFIG
+        self.config = self.DEFAULT_CONFIG.copy() # create instance specific setting
 
         # Try to get valid config from settings.
         configs = getattr(settings, 'CKEDITOR_CONFIGS', None)
@@ -56,10 +57,17 @@ class CKEditorWidget(forms.Textarea):
                 raise ImproperlyConfigured('CKEDITOR_CONFIGS setting must be a dictionary type.')
             
     def render(self, name, value, attrs={}):
+        # Finish setting up DEFAULT_CONFIG - we do it here as in __init__ urls are not defined yet 
+        # also we can't use lambda to try to put it in __init__ as json_encode dosen't know how to handle it
+        try: 
+            self.DEFAULT_CONFIG['filebrowserUploadUrl']
+        except KeyError:
+            self.DEFAULT_CONFIG['filebrowserUploadUrl'] = reverse('ckeditor_upload') # shared among class
+            self.DEFAULT_CONFIG['filebrowserBrowseUrl'] = reverse('ckeditor_browse') # shared among class
+        
+        
         if value is None: value = ''
         final_attrs = self.build_attrs(attrs, name=name)
-        self.config['filebrowserUploadUrl'] = reverse('ckeditor_upload')
-        self.config['filebrowserBrowseUrl'] = reverse('ckeditor_browse')
         return mark_safe(u'''<textarea%s>%s</textarea>
         <script type="text/javascript">
             CKEDITOR.replace("%s", %s);
