dd7d0220b6ba00b832d5658e27b2030f3392df5f
Shaun Sephton <shaun@28lines.com>
2012-04-22 14:29:48 +0200

    use static

--- project/settings.py
+++ project/settings.py
@@ -62,7 +62,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PATH, "static")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -153,7 +153,6 @@ LOGGING = {
     }
 }
 
-CKEDITOR_MEDIA_PREFIX = "/media/ckeditor/"
 CKEDITOR_UPLOAD_PATH = PATH + "/media/uploads"
 
 CKEDITOR_CONFIGS = {
