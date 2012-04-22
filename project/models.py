1bb3a913ffbfbdbf1de2c6213feac15e3ccdc26a
Shaun Sephton <shaun@28lines.com>
2012-04-22 13:01:10 +0200

    added verbose names

--- project/models.py
+++ project/models.py
@@ -3,6 +3,6 @@ from ckeditor.fields import RichTextField
 
 
 class Post(models.Model):
-    content_basic = RichTextField(config_name='basic')
-    content_full = RichTextField(config_name='full')
-    content_custom = RichTextField(config_name='custom')
+    content_basic = RichTextField('Basic', config_name='basic')
+    content_full = RichTextField('Full', config_name='full')
+    content_custom = RichTextField(config_name='custom', verbose_name='Custom')
