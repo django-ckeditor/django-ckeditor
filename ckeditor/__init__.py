d2acc1fd98160c59eb88d59e431c6d2225cf3d41
Shaun Sephton <shaun@28lines.com>
2012-04-22 14:32:14 +0200

    no longer validate CKEDITOR_MEDIA_PREFIX as it is no longer used with static

--- ckeditor/__init__.py
+++ ckeditor/__init__.py
@@ -4,16 +4,6 @@ from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
 if 'ckeditor' in settings.INSTALLED_APPS:
-    # Confirm CKEDITOR_MEDIA_PREFIX setting has been specified.
-    try:
-        settings.CKEDITOR_MEDIA_PREFIX
-    except AttributeError:
-        raise ImproperlyConfigured("django-ckeditor requires \
-                CKEDITOR_MEDIA_PREFIX setting. This setting specifies a URL \
-                prefix to the ckeditor JS and CSS media (not uploaded media). \
-                Make sure to use a trailing slash: CKEDITOR_MEDIA_PREFIX = \
-                '/media/ckeditor/'")
-
     # Confirm CKEDITOR_UPLOAD_PATH setting has been specified.
     try:
         settings.CKEDITOR_UPLOAD_PATH
