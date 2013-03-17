6168bf76ec39d3639f8fed59b9bd86506afdb2d8
Lihan Li <lilihan.it@gmail.com>
2013-03-17 16:54:34 +1100

    Replaced soon to be deprecated simplejson package

--- ckeditor/widgets.py
+++ ckeditor/widgets.py
@@ -5,12 +5,12 @@ from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe
 from django.utils.html import conditional_escape
 from django.utils.encoding import force_unicode
-from django.utils import simplejson
-
 from django.core.exceptions import ImproperlyConfigured
 from django.forms.util import flatatt
+import json
 
-json_encode = simplejson.JSONEncoder().encode
+
+json_encode = json.JSONEncoder().encode
 
 DEFAULT_CONFIG = {
     'skin': 'moono',
