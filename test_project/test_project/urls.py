64293849b3c982e46beb73c07c6da4a468906d38
Shaun Sephton <connect@shaunsephton.com>
2013-04-04 08:34:20 +0200

    "server media"

--- test_project/test_project/urls.py
+++ test_project/test_project/urls.py
@@ -1,8 +1,19 @@
-from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls import patterns, include
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     (r'^admin/', include(admin.site.urls)),
     (r'^ckeditor/', include('ckeditor.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        '',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+         {'document_root': settings.MEDIA_ROOT}),
+    )
+
+import pdb; pdb.set_trace()
