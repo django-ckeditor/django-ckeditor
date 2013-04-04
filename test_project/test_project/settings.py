3fa1a16f1706ea02567297f34706a71714fc8ddb
Shaun Sephton <connect@shaunsephton.com>
2013-04-04 08:34:13 +0200

    "corrected meida paths"

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -55,12 +55,12 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = PATH + "/media/"
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
