f70369c83973cf6f0885f05acbb45c2422137527
Rocio Aramberri <roschegel@gmail.com>
2013-09-02 23:13:39 -0300

    Fixed deprecation warning in import of django.conf.urls.defaults

--- ckeditor/urls.py
+++ ckeditor/urls.py
@@ -1,4 +1,9 @@
-from django.conf.urls.defaults import patterns, url
+import django
+
+if float("%d.%d"%(django.VERSION[0],django.VERSION[1])) < 1.5:
+    from django.conf.urls.defaults import patterns,url
+else:    
+    from django.conf.urls import patterns,url
 
 urlpatterns = patterns(
     '',
