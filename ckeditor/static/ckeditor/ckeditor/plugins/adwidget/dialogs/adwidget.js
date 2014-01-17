8a78c759eba1a60188e1e757dbcc85e69fd40657
Miriam Sexton <miriam@industrydive.com>
2014-01-17 12:21:48 -0500

    step 1 toward ad floating

--- ckeditor/static/ckeditor/ckeditor/plugins/adwidget/dialogs/adwidget.js
+++ ckeditor/static/ckeditor/ckeditor/plugins/adwidget/dialogs/adwidget.js
@@ -24,7 +24,29 @@ CKEDITOR.dialog.add( 'adwidget', function( editor ) {
 						commit: function(widget) {
 							widget.setData('current_ad_type', this.getValue());
 						}
-
+					},
+					{
+						id: 'ad_style',
+						type: 'select',
+						label: 'Choose Ad Style',
+						items: [
+							['On the Right', 'float_right'],
+							['Centered,  no text wrapping', 'centered'],
+						],
+						setup: function(widget) {
+							if ( widget.element.hasClass('float_right') ) {
+								this.setValue('float_right');
+							} else {
+								this.setValue('centered');
+							}
+						},
+						commit: function(widget) {
+							if ( ! widget.element.hasClass(this.getValue()) ) {
+								widget.element.removeClass('float_right');
+								widget.element.removeClass('centered');
+								widget.element.addClass(this.getValue());
+							}
+						}
 					}
 				]
 			}
