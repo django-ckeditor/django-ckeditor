df50dc82470bda8891bc3d99bcb09ceec465623e
Paritskiy Vitaliy <crunching.karma@gmail.com>
2014-02-04 19:02:28 +0200

    Update urls.py
    Updated urls.py import statement because django.conf.urls.defaults is deprecated since django 1.6

--- ckeditor/urls.py
+++ ckeditor/urls.py
@@ -1,4 +1,8 @@
-from django.conf.urls.defaults import patterns, url
+import django
+if django.VERSION[0:2] >= (1, 4):
+    from django.conf.urls import patterns, url
+else:
+    from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns(
     '',
