09ee04f22d4c3a086928b76a6a835502f9ec0a8f
Piotr Maliński <riklaunim@gmail.com>
2014-06-01 00:53:29 +0200

    Update ckeditor to 4.4.1.

--- ckeditor/static/ckeditor/ckeditor/build-config.js
+++ ckeditor/static/ckeditor/ckeditor/build-config.js
@@ -13,10 +13,10 @@
  * (1) http://ckeditor.com/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) http://ckeditor.com/builder/97b905d3c8091c5a104b06cf745f12bd
+ * (2) http://ckeditor.com/builder/e41bccb8290b6d530f8478ddafe95c48
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) http://ckeditor.com/builder/download/97b905d3c8091c5a104b06cf745f12bd
+ * (3) http://ckeditor.com/builder/download/e41bccb8290b6d530f8478ddafe95c48
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -26,7 +26,7 @@
 
 var CKBUILDER_CONFIG = {
 	skin: 'moono',
-	preset: 'full',
+	preset: 'standard',
 	ignore: [
 		'dev',
 		'.gitignore',
@@ -38,76 +38,39 @@ var CKBUILDER_CONFIG = {
 		'a11yhelp' : 1,
 		'about' : 1,
 		'basicstyles' : 1,
-		'bidi' : 1,
 		'blockquote' : 1,
-		'button' : 1,
 		'clipboard' : 1,
-		'colorbutton' : 1,
-		'colordialog' : 1,
 		'contextmenu' : 1,
-		'dialog' : 1,
-		'dialogadvtab' : 1,
-		'dialogui' : 1,
-		'div' : 1,
 		'elementspath' : 1,
 		'enterkey' : 1,
 		'entities' : 1,
-		'fakeobjects' : 1,
 		'filebrowser' : 1,
-		'find' : 1,
-		'flash' : 1,
 		'floatingspace' : 1,
-		'floatpanel' : 1,
-		'font' : 1,
 		'format' : 1,
-		'forms' : 1,
 		'horizontalrule' : 1,
 		'htmlwriter' : 1,
-		'iframe' : 1,
 		'image' : 1,
-		'indent' : 1,
-		'indentblock' : 1,
 		'indentlist' : 1,
-		'justify' : 1,
 		'link' : 1,
 		'list' : 1,
-		'listblock' : 1,
-		'liststyle' : 1,
 		'magicline' : 1,
 		'maximize' : 1,
-		'menu' : 1,
-		'menubutton' : 1,
-		'newpage' : 1,
-		'pagebreak' : 1,
-		'panel' : 1,
-		'panelbutton' : 1,
 		'pastefromword' : 1,
 		'pastetext' : 1,
-		'popup' : 1,
-		'preview' : 1,
-		'print' : 1,
 		'removeformat' : 1,
 		'resize' : 1,
-		'richcombo' : 1,
-		'save' : 1,
 		'scayt' : 1,
-		'selectall' : 1,
-		'showblocks' : 1,
 		'showborders' : 1,
-		'smiley' : 1,
 		'sourcearea' : 1,
 		'specialchar' : 1,
 		'stylescombo' : 1,
-		'stylesheetparser' : 1,
 		'tab' : 1,
 		'table' : 1,
 		'tabletools' : 1,
-		'templates' : 1,
 		'toolbar' : 1,
 		'undo' : 1,
 		'wsc' : 1,
-		'wysiwygarea' : 1,
-		'youtube' : 1
+		'wysiwygarea' : 1
 	},
 	languages : {
 		'af' : 1,
@@ -170,6 +133,7 @@ var CKBUILDER_CONFIG = {
 		'sv' : 1,
 		'th' : 1,
 		'tr' : 1,
+		'tt' : 1,
 		'ug' : 1,
 		'uk' : 1,
 		'vi' : 1,
