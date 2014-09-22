89c9f78d72a1149c9f6bfa260ad04afa68b04080
Piotr Maliński <riklaunim@gmail.com>
2014-09-23 00:23:21 +0200

    Refactor ckeditor-init.js.

--- ckeditor/static/ckeditor/ckeditor-init.js
+++ ckeditor/static/ckeditor/ckeditor-init.js
@@ -1,19 +1,20 @@
 $(function() {
+    initialiseCKEditor();
+    initialiseCKEditorInInlinedForms();
 
-    function initCKEditor(){
-        $('textarea[data-type=ckeditortype]').each(function(){
-            if($(this).data('processed') == "0" && $(this).attr('id').indexOf('__prefix__') == -1){
-                $(this).data('processed',"1");
-                CKEDITOR.replace($(this).attr('id'), $(this).data('config'));
-            }
+    function initialiseCKEditorInInlinedForms() {
+        $(".add-row a").click(function () {
+            initialiseCKEditor();
+            return true;
         });
     }
-
-    initCKEditor();
-
-    $(".add-row a").click(function(){
-        initCKEditor();
-        return true;
-    });
-
 });
+
+function initialiseCKEditor() {
+    $('textarea[data-type=ckeditortype]').each(function(){
+        if($(this).data('processed') == "0" && $(this).attr('id').indexOf('__prefix__') == -1){
+            $(this).data('processed',"1");
+            CKEDITOR.replace($(this).attr('id'), $(this).data('config'));
+        }
+    });
+}
