26a00bdedbc8f4bc0ac88f220938871bab135f53
Piotr Maliński <riklaunim@gmail.com>
2015-09-06 03:02:27 +0200

    Refactor ImageUploadView.

--- ckeditor/views.py
+++ ckeditor/views.py
@@ -42,23 +42,12 @@ class ImageUploadView(generic.View):
         """
         Uploads a file and send back its URL to CKEditor.
         """
-        # Get the uploaded file from request.
-        upload = request.FILES['upload']
+        uploaded_file = request.FILES['upload']
 
-        # Verify that file is a valid image
         backend = image_processing.get_backend()
-        try:
-            backend.image_verify(upload)
-        except utils.NotAnImageException:
-            pass
-
-        # Open output file in which to store upload.
-        upload_filename = get_upload_filename(upload.name, request.user)
-        saved_path = default_storage.save(upload_filename, upload)
-
-        if backend.should_create_thumbnail(saved_path):
-            backend.create_thumbnail(saved_path)
-
+        self._verify_file(backend, uploaded_file)
+        saved_path = self._save_file(request, uploaded_file)
+        self._create_thumbnail_if_needed(backend, saved_path)
         url = utils.get_media_url(saved_path)
 
         # Respond with Javascript sending ckeditor upload url.
@@ -66,6 +55,27 @@ class ImageUploadView(generic.View):
         <script type='text/javascript'>
             window.parent.CKEDITOR.tools.callFunction({0}, '{1}');
         </script>""".format(request.GET['CKEditorFuncNum'], url))
+
+    def _verify_file(self, backend, uploaded_file):
+        try:
+            backend.image_verify(uploaded_file)
+        except utils.NotAnImageException:
+            return self._on_verification_failure()
+
+    def _on_verification_failure(self):
+        pass
+
+    @staticmethod
+    def _save_file(request, uploaded_file):
+        filename = get_upload_filename(uploaded_file.name, request.user)
+        saved_path = default_storage.save(filename, uploaded_file)
+        return saved_path
+
+    @staticmethod
+    def _create_thumbnail_if_needed(backend, saved_path):
+        if backend.should_create_thumbnail(saved_path):
+            backend.create_thumbnail(saved_path)
+
 
 upload = csrf_exempt(ImageUploadView.as_view())
 
