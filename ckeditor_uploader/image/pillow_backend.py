3baf22af25fa4cf0df25821019e7e662b80a0daa
hairetdin <amir.hairetdinov@gmail.com>
2016-01-27 16:48:15 +0500

    - Function should_create_thumbnail renamed to is_image. It only checked image file. - Function resize_uploaded_image added for risize image size if set CKEDITOR_SAVED_IMAGE_LENGTH_MAX setting.

--- ckeditor_uploader/image/pillow_backend.py
+++ ckeditor_uploader/image/pillow_backend.py
@@ -56,7 +56,7 @@ def create_thumbnail(file_path):
     return default_storage.save(thumbnail_filename, thumbnail)
 
 
-def should_create_thumbnail(file_path):
+def is_image(file_path):
     image = default_storage.open(file_path)
     try:
         Image.open(image)
@@ -64,3 +64,34 @@ def should_create_thumbnail(file_path):
         return False
     else:
         return utils.is_valid_image_extension(file_path)
+
+        
+def resize_uploaded_image(file_path, image_longest_side):
+    # if settings CKEDITOR_SAVED_IMAGE_LENGTH_MAX as string
+    image_longest_side = int(image_longest_side)
+    # adding _resized to filename
+    resizing_filename = utils.get_resize_filename(file_path)
+    # read image from saved_path
+    image = default_storage.open(file_path)
+    image = Image.open(image)
+    img_format = image.format
+
+    # set image size (from settings CKEDITOR_SAVED_IMAGE_LENGTH_MAX)
+    width, height = image.size
+    proportion = width/height
+    if width > height:
+        new_width = image_longest_side
+        new_height = new_width/proportion
+    else:
+        new_height = image_longest_side
+        new_width = new_height*proportion
+ 
+    resizedImage = image.resize((int(new_width), int(new_height)), Image.ANTIALIAS)
+    resizedImageFile = BytesIO()
+    resizedImage.save(resizedImageFile, img_format)
+
+    # create InMemoryUploadedFile image object
+    resized_image = InMemoryUploadedFile(resizedImageFile, None, resizing_filename, img_format, len(resizedImageFile.getvalue()), None)
+    resizedImageFile.seek(0)
+    
+    return default_storage.save(resizing_filename, resized_image)
