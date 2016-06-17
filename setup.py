e116edeee57d97a0b4ae70d9e012bdf0945986d1
Nik Nyby <nnyby@columbia.edu>
2016-06-17 16:52:23 -0400

    add python 3.5 classifier to setup.py

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Framework :: Django",
