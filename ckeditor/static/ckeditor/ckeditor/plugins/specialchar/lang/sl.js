a730c4ed34298e62ab19c263749427f15b7952ba
rschuster3 <rschuster@industrydive.com>
2016-08-08 12:47:36 -0400

    add specialchar plugin

---
+++ ckeditor/static/ckeditor/ckeditor/plugins/specialchar/lang/sl.js
@@ -0,0 +1,9 @@
+/*
+Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.md or http://ckeditor.com/license
+*/
+CKEDITOR.plugins.setLang( 'specialchar', 'sl', {
+	options: 'Možnosti posebnih znakov',
+	title: 'Izberi posebni znak',
+	toolbar: 'Vstavi posebni znak'
+} );
