229ac1e1ad3a35f8ec5f9b3f930775440bb938b0
Piotr Maliński <riklaunim@gmail.com>
2016-08-22 00:30:07 +0200

    Django 1.10 compatibility.

--- ckeditor_demo/urls.py
+++ ckeditor_demo/urls.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 import django
 from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles import views
 
@@ -13,11 +14,7 @@ if django.VERSION >= (1, 8):
         url(r'^$', ckeditor_form_view, name='ckeditor-form'),
         url(r'^admin/', include(admin.site.urls)),
         url(r'^ckeditor/', include('ckeditor_uploader.urls')),
-        url(r'^static/(?P<path>.*)$', views.serve),
-        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
-            'document_root': settings.MEDIA_ROOT,
-        }),
-    ]
+    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 else:
     from django.conf.urls import patterns
 
