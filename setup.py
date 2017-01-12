4642874b80312cb4de4b835dac35885ef4ee0130
Smite Chow <xiaopengyou2no1@gmail.com>
2017-01-12 17:43:55 +0800

    bump 5.3.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 import sys
 from setuptools import setup, find_packages
 
-version = '5.2.1'
+version = '5.3.1'
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
