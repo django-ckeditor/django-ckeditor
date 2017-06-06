4e5cd130ffb7773864be96a827d05ea743f691a9
kidig <kiddima@gmail.com>
2017-06-06 15:49:43 +0300

    add ru locale to the ckeditor_uploader

--- ckeditor_uploader/forms.py
+++ ckeditor_uploader/forms.py
@@ -1,7 +1,8 @@
 from __future__ import absolute_import
 
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 
 class SearchForm(forms.Form):
-    q = forms.CharField(label='Search files', required=False)
+    q = forms.CharField(label=_('Search files'), required=False)
