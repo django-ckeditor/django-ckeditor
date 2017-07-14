caaa7abee6ac0b90e6da4c31849b6ae1b1aeea3c
Brechbühler Gil <Gil.Brechbuhler@epfl.ch>
2017-07-14 13:04:28 +0200

    Add a plugin to embed tweets (#1)
    * add embed plugin
    
    * embed needs embedbase
    
    * add missing dependancies
    
    * add lineutils dependancy for widgets
    
    * add blockquotes plugin
    
    * use noembed instead of iframe, as noembed is free
    
    * add callback handling
    
    * noembed does not work, try twitter oembed directly
    
    * try noembed again
    
    * reset embed plugin config at default
    
    * specialize embed plugin for twitter
    
    * better twitter icon
    
    * gray -> black for twitter icon
    
    * test without maybe-useless plugin
    
    * use only twitter provider in twitterembed plugin
    
    * remove media-embed icons, we only neeed twitter ones
    
    * update twitter button label
    
    * add language declaration in plugin definintion
    
    * declare plugin language with correct plugin name
    
    * little test for framed text translation
    
    * Revert "little test for framed text translation"
    
    This reverts commit ed1bcb43722130d7e5a79fa5ef2aa4d43a156539.
    
    * remove unneeded comment

---
+++ ckeditor/static/ckeditor/ckeditor/plugins/twitterembed/lang/fr.js
@@ -0,0 +1,9 @@
+/*
+Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.md or http://ckeditor.com/license
+*/
+CKEDITOR.plugins.setLang( 'twitterembed', 'fr', {
+	pathName: 'objet média',
+	title: 'Incorporation de tweet',
+	button: 'Incorporer un tweet',
+} );
---
+++ ckeditor/static/ckeditor/ckeditor/plugins/twitterembed/plugin.js
@@ -0,0 +1,82 @@
+/**
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+( function() {
+	'use strict';
+
+	CKEDITOR.plugins.add( 'twitterembed', {
+		icons: 'twitterembed', // %REMOVE_LINE_CORE%
+		hidpi: true, // %REMOVE_LINE_CORE%
+		lang: 'en,fr',
+		requires: 'embedbase',
+
+		init: function( editor ) {
+			var widgetDefinition = CKEDITOR.plugins.embedBase.createWidgetBaseDefinition( editor );
+
+			// Extend the base definition with additional properties.
+			CKEDITOR.tools.extend( widgetDefinition, {
+				// Use a dialog exposed by the embedbase plugin.
+				dialog: 'embedBase',
+				button: editor.lang.twitterembed.button,
+				allowedContent: 'div[!data-oembed-url]',
+				requiredContent: 'div[data-oembed-url]',
+				providerUrl: new CKEDITOR.template(
+					'//publish.twitter.com/oembed?url={url}&callback={callback}'
+				),
+
+				// The filter element callback actually allows all divs with data-oembed-url,
+				// so registering styles to the filter is virtually unnecessary because
+				// classes won't be filtered out. However, registering them will make filter.check() work
+				// which may be important in some cases.
+				styleToAllowedContentRules: function( style ) {
+					// Retrieve classes defined in the style.
+					var classes = style.getClassesArray();
+
+					return {
+						div: {
+							propertiesOnly: true,
+							classes: classes,
+							attributes: '!data-oembed-url'
+						}
+					};
+				},
+
+				upcast: function( el, data ) {
+					if ( el.name == 'div' && el.attributes[ 'data-oembed-url' ] ) {
+						data.url = el.attributes[ 'data-oembed-url' ];
+
+						return true;
+					}
+				},
+
+				downcast: function( el ) {
+					el.attributes[ 'data-oembed-url' ] = this.data.url;
+				}
+			}, true );
+
+			// Register the definition as 'embed' widget.
+			editor.widgets.add( 'twitterembed', widgetDefinition );
+
+			// Do not filter contents of the div[data-oembed-url] at all.
+			editor.filter.addElementCallback( function( el ) {
+				if ( 'data-oembed-url' in el.attributes ) {
+					return CKEDITOR.FILTER_SKIP_TREE;
+				}
+			} );
+		}
+	} );
+
+} )();
+
+/**
+ * Read more in the [documentation](#!/guide/dev_media_embed)
+ * and see the [SDK sample](http://sdk.ckeditor.com/samples/mediaembed.html).
+ *
+ * Refer to {@link CKEDITOR.plugins.embedBase.baseDefinition#providerUrl} for more information about content providers.
+ *
+ * @since 4.5
+ * @cfg {String} [embed_provider=//ckeditor.iframe.ly/api/oembed?url={url}&callback={callback}]
+ * @member CKEDITOR.config
+ */
