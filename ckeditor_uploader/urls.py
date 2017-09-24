7803de1959dea4f7ab4f405cb4c71839c7ccb00c
Diego Fernando <diegofernando5672@gmail.com>
2017-09-24 07:33:55 -0300

    modification of upload rules
    modification of upload rules to allow authenticated users to upload files

--- ckeditor_uploader/urls.py
+++ ckeditor_uploader/urls.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 
 import django
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 from django.views.decorators.cache import never_cache
 
@@ -9,13 +10,13 @@ from . import views
 
 if django.VERSION >= (1, 8):
     urlpatterns = [
-        url(r'^upload/', staff_member_required(views.upload), name='ckeditor_upload'),
-        url(r'^browse/', never_cache(staff_member_required(views.browse)), name='ckeditor_browse'),
+        url(r'^upload/', login_required(views.upload), name='ckeditor_upload'),
+        url(r'^browse/', never_cache(login_required(views.browse)), name='ckeditor_browse'),
     ]
 else:
     from django.conf.urls import patterns
     urlpatterns = patterns(
         '',
-        url(r'^upload/', staff_member_required(views.upload), name='ckeditor_upload'),
+        url(r'^upload/', login_required(views.upload), name='ckeditor_upload'),
         url(r'^browse/', never_cache(staff_member_required(views.browse)), name='ckeditor_browse'),
     )
