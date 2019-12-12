d4049cb972a2f92145a81f8697735abf222bf764
Dabble <ddabble@users.noreply.github.com>
2019-12-12 01:15:14 +0100

    Code cleanup

--- ckeditor_demo/demo_application/tests/test_deprecation.py
+++ ckeditor_demo/demo_application/tests/test_deprecation.py
@@ -2,25 +2,25 @@ import warnings
 
 from django.contrib.auth.models import User
 from django.http.request import HttpRequest
-from django.test import TransactionTestCase
+from django.test import TestCase
 from django.test.utils import override_settings
 
 from ckeditor_uploader.views import get_upload_filename
 from .utils import get_absolute_name
 
-mock_filename = 'some_filename.jpg'
+MOCK_FILENAME = 'some_filename.jpg'
 mock_request = HttpRequest()
 
 
 def generator_with_filename_and_request(filename, request):
-    assert filename == mock_filename
+    assert filename == MOCK_FILENAME
     assert request is mock_request
     assert hasattr(request, 'user')
     return filename
 
 
 def generator_with_only_filename(filename):
-    assert filename == mock_filename
+    assert filename == MOCK_FILENAME
     return filename
 
 
@@ -28,33 +28,27 @@ def generator_without_parameters():
     pass
 
 
-class TestUploadFilenameGeneratorParameters(TransactionTestCase):
+class TestUploadFilenameGeneratorParameters(TestCase):
     fixtures = ['test_admin.json']
 
-    @classmethod
-    def setUpClass(cls):
-        super(TestUploadFilenameGeneratorParameters, cls).setUpClass()
-
-    @classmethod
-    def tearDownClass(cls):
-        super(TestUploadFilenameGeneratorParameters, cls).tearDownClass()
-
     def setUp(self):
-        user = User.objects.get(pk=1)
-        mock_request.user = user
+        mock_request.user = User.objects.get(pk=1)
 
     @override_settings(CKEDITOR_FILENAME_GENERATOR=get_absolute_name(generator_with_filename_and_request))
     def test_compatible_parameters(self):
+        # `self.assertWarns()` is not available in Python 2
         with warnings.catch_warnings(record=True) as w:
             warnings.simplefilter("always")
-            get_upload_filename(mock_filename, mock_request)
+
+            get_upload_filename(MOCK_FILENAME, mock_request)
             assert len(w) == 0
 
     @override_settings(CKEDITOR_FILENAME_GENERATOR=get_absolute_name(generator_with_only_filename))
     def test_no_request_parameter_deprecation_warning(self):
         with warnings.catch_warnings(record=True) as w:
             warnings.simplefilter("always")
-            get_upload_filename(mock_filename, mock_request)
+
+            get_upload_filename(MOCK_FILENAME, mock_request)
             assert len(w) == 1
             assert issubclass(w[0].category, PendingDeprecationWarning)
 
@@ -62,5 +56,6 @@ class TestUploadFilenameGeneratorParameters(TransactionTestCase):
     def test_incompatible_parameters(self):
         with warnings.catch_warnings(record=True) as w:
             warnings.simplefilter("always")
-            get_upload_filename(mock_filename, mock_request)
+
+            get_upload_filename(MOCK_FILENAME, mock_request)
             assert len(w) == 1
