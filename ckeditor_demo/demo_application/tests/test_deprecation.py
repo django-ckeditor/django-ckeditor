c5ff6209c3ab68af4788277b2ba7d6c6eabd03e6
Dabble <ddabble@users.noreply.github.com>
2019-12-12 01:24:41 +0100

    Replaced assert keyword with self.assertX()
    Also added some asserts on the generated filename, and improved creating a mock request.

--- ckeditor_demo/demo_application/tests/test_deprecation.py
+++ ckeditor_demo/demo_application/tests/test_deprecation.py
@@ -1,38 +1,36 @@
+import os
 import warnings
 
 from django.contrib.auth.models import User
-from django.http.request import HttpRequest
-from django.test import TestCase
+from django.test import RequestFactory, TestCase
 from django.test.utils import override_settings
 
 from ckeditor_uploader.views import get_upload_filename
 from .utils import get_absolute_name
 
 MOCK_FILENAME = 'some_filename.jpg'
-mock_request = HttpRequest()
+GENERATOR_PREFIX = 'some_folder'
+GENERATED_MOCK_FILENAME = os.path.join(GENERATOR_PREFIX, MOCK_FILENAME)
 
 
-def generator_with_filename_and_request(filename, request):
-    assert filename == MOCK_FILENAME
-    assert request is mock_request
-    assert hasattr(request, 'user')
-    return filename
+def generator_with_filename_and_request(filename, _request):
+    return os.path.join(GENERATOR_PREFIX, filename)
 
 
 def generator_with_only_filename(filename):
-    assert filename == MOCK_FILENAME
-    return filename
+    return os.path.join(GENERATOR_PREFIX, filename)
 
 
 def generator_without_parameters():
-    pass
+    return GENERATOR_PREFIX
 
 
 class TestUploadFilenameGeneratorParameters(TestCase):
     fixtures = ['test_admin.json']
 
     def setUp(self):
-        mock_request.user = User.objects.get(pk=1)
+        self.mock_request = RequestFactory().request()
+        self.mock_request.user = User.objects.get(username='test')
 
     @override_settings(CKEDITOR_FILENAME_GENERATOR=get_absolute_name(generator_with_filename_and_request))
     def test_compatible_parameters(self):
@@ -40,22 +38,29 @@ class TestUploadFilenameGeneratorParameters(TestCase):
         with warnings.catch_warnings(record=True) as w:
             warnings.simplefilter("always")
 
-            get_upload_filename(MOCK_FILENAME, mock_request)
-            assert len(w) == 0
+            generated_filename = get_upload_filename(MOCK_FILENAME, self.mock_request)
+            self.assertEqual(len(w), 0)
+
+        self.assertIn(GENERATED_MOCK_FILENAME, generated_filename)
 
     @override_settings(CKEDITOR_FILENAME_GENERATOR=get_absolute_name(generator_with_only_filename))
     def test_no_request_parameter_deprecation_warning(self):
         with warnings.catch_warnings(record=True) as w:
             warnings.simplefilter("always")
 
-            get_upload_filename(MOCK_FILENAME, mock_request)
-            assert len(w) == 1
-            assert issubclass(w[0].category, PendingDeprecationWarning)
+            generated_filename = get_upload_filename(MOCK_FILENAME, self.mock_request)
+            self.assertEqual(len(w), 1)
+            self.assertTrue(issubclass(w[0].category, PendingDeprecationWarning))
+
+        self.assertIn(GENERATED_MOCK_FILENAME, generated_filename)
 
     @override_settings(CKEDITOR_FILENAME_GENERATOR=get_absolute_name(generator_without_parameters))
     def test_incompatible_parameters(self):
         with warnings.catch_warnings(record=True) as w:
             warnings.simplefilter("always")
 
-            get_upload_filename(MOCK_FILENAME, mock_request)
-            assert len(w) == 1
+            generated_filename = get_upload_filename(MOCK_FILENAME, self.mock_request)
+            self.assertEqual(len(w), 1)
+
+        self.assertIn(MOCK_FILENAME, generated_filename)
+        self.assertNotIn(GENERATOR_PREFIX, generated_filename)
