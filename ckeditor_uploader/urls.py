c89353ea3d393be4d9b16b8071c82e493c237bc8
Diego Fernando <diegofelima.ti@gmail.com>
2020-10-03 22:52:09 -0300

    Changing 'django.conf.urls.url()' to 'django.urls.re_path()' because of depreciation

--- ckeditor_uploader/urls.py
+++ ckeditor_uploader/urls.py
@@ -1,14 +1,14 @@
 from __future__ import absolute_import
 
-from django.conf.urls import url
+from django.urls import re_path
 from django.contrib.admin.views.decorators import staff_member_required
 from django.views.decorators.cache import never_cache
 
 from . import views
 
 urlpatterns = [
-    url(r"^upload/", staff_member_required(views.upload), name="ckeditor_upload"),
-    url(
+    re_path(r"^upload/", staff_member_required(views.upload), name="ckeditor_upload"),
+    re_path(
         r"^browse/",
         never_cache(staff_member_required(views.browse)),
         name="ckeditor_browse",
