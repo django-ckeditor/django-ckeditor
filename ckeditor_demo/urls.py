3951b726013f5c4ae465e907fba575f9b418bb95
Diego Fernando <diegofernando5672@gmail.com>
2020-10-04 04:18:40 -0300

    Changing 'django.conf.urls.url()' to 'django.urls.re_path()' because of depreciation (#635)
    Co-authored-by: Diego Fernando <diegofelima.ti@gmail.com>

--- ckeditor_demo/urls.py
+++ ckeditor_demo/urls.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
 from django.conf import settings
-from django.conf.urls import include, url
+from django.urls import re_path, include
 from django.conf.urls.static import static
 from django.contrib import admin
 
@@ -9,9 +9,9 @@ from .demo_application.views import ckeditor_form_view
 
 urlpatterns = (
     [
-        url(r"^$", ckeditor_form_view, name="ckeditor-form"),
-        url(r"^admin/", admin.site.urls),
-        url(r"^ckeditor/", include("ckeditor_uploader.urls")),
+        re_path(r"^$", ckeditor_form_view, name="ckeditor-form"),
+        re_path(r"^admin/", admin.site.urls),
+        re_path(r"^ckeditor/", include("ckeditor_uploader.urls")),
     ]
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
