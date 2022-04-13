11c68673d09c308ac9ebcc008bcb03d053941be3
Dabble <ddabble@users.noreply.github.com>
2022-04-13 15:53:57 +0200

    Added tests for Django admin model forms

--- ckeditor_demo/demo_application/tests/test_fields.py
+++ ckeditor_demo/demo_application/tests/test_fields.py
@@ -1,4 +1,8 @@
+from http import HTTPStatus
+
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
 from ckeditor.fields import RichTextFormField
 from ckeditor.widgets import CKEditorWidget
@@ -14,20 +18,23 @@ from ..forms import (
 class ModelFieldAndFormFieldTestCase(TestCase):
     def test_non_upload_model_form_contains_expected_formfield(self):
         form = ExampleNonUploadModelForm()
-        form_field = form.fields["content"]
-        self.assertIs(type(form_field), RichTextFormField)
-        widget = form_field.widget
-        self.assertIs(type(widget), CKEditorWidget)
-        self.assertEqual(widget.config_name, "default")
-        self.assertEqual(widget.config.get("extraPlugins"), None)
-        self.assertListEqual(widget.external_plugin_resources, [])
+        self.assert_form_contains_expected_formfield_with_default_widget_options(
+            form, "content", RichTextFormField, CKEditorWidget
+        )
 
     def test_upload_model_form_contains_expected_formfield(self):
         form = ExampleModelForm()
-        form_field = form.fields["content"]
-        self.assertIs(type(form_field), RichTextUploadingFormField)
+        self.assert_form_contains_expected_formfield_with_default_widget_options(
+            form, "content", RichTextUploadingFormField, CKEditorUploadingWidget
+        )
+
+    def assert_form_contains_expected_formfield_with_default_widget_options(
+        self, form, field_name, expected_field_type, expected_widget_type
+    ):
+        form_field = form.fields[field_name]
+        self.assertIs(type(form_field), expected_field_type)
         widget = form_field.widget
-        self.assertIs(type(widget), CKEditorUploadingWidget)
+        self.assertIs(type(widget), expected_widget_type)
         self.assertEqual(widget.config_name, "default")
         self.assertEqual(widget.config.get("extraPlugins"), None)
         self.assertListEqual(widget.external_plugin_resources, [])
@@ -49,4 +56,28 @@ class ModelFieldAndFormFieldTestCase(TestCase):
                     "plugin.js",
                 )
             ],
+        )
+
+    def test_admin_model_forms_contain_expected_formfields(self):
+        superuser = get_user_model().objects.create_user(
+            username="superuser", is_superuser=True, is_staff=True
+        )
+        self.client.force_login(superuser)
+
+        response = self.client.get(
+            reverse("admin:demo_application_examplenonuploadmodel_add")
+        )
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+        form = response.context["adminform"].form
+        self.assert_form_contains_expected_formfield_with_default_widget_options(
+            form, "content", RichTextFormField, CKEditorWidget
+        )
+
+        response = self.client.get(reverse("admin:demo_application_examplemodel_add"))
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+        form = response.context["adminform"].form
+        self.assert_form_contains_expected_formfield_with_default_widget_options(
+            form, "content", RichTextUploadingFormField, CKEditorUploadingWidget
         )
