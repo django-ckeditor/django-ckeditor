b71c85be2974d8bca0ee8562cf2fcaacca8e8749
Hamid Rasti <43915620+hamidrasti@users.noreply.github.com>
2024-06-16 17:13:50 +0330

    chore: fixed typo in warning message (#772)

--- ckeditor/apps.py
+++ ckeditor/apps.py
@@ -5,7 +5,7 @@ from django.core.checks import Warning, register
 def check_ckeditor_version(app_configs, **kwargs):
     return [
         Warning(
-            "django-ckeditor bundles CKEditor 4.22.1 which isn't supported anmyore and which does have unfixed security issues, see for example https://ckeditor.com/cke4/release/CKEditor-4.24.0-LTS . You should consider strongly switching to a different editor (maybe CKEditor 5 respectively django-ckeditor-5 after checking whether the CKEditor 5 license terms work for you) or switch to the non-free CKEditor 4 LTS package. See https://ckeditor.com/ckeditor-4-support/ for more on this. (Note! This notice has been added by the django-ckeditor developers and we are not affiliated with CKSource and were not involved in the licensing change, so please refrain from complaining to us. Thanks.)",
+            "django-ckeditor bundles CKEditor 4.22.1 which isn't supported anymore and which does have unfixed security issues, see for example https://ckeditor.com/cke4/release/CKEditor-4.24.0-LTS . You should consider strongly switching to a different editor (maybe CKEditor 5 respectively django-ckeditor-5 after checking whether the CKEditor 5 license terms work for you) or switch to the non-free CKEditor 4 LTS package. See https://ckeditor.com/ckeditor-4-support/ for more on this. (Note! This notice has been added by the django-ckeditor developers and we are not affiliated with CKSource and were not involved in the licensing change, so please refrain from complaining to us. Thanks.)",
             id="ckeditor.W001",
         )
     ]
